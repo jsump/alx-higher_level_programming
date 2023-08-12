1c0ca87910e594dc12e2f06cf443412d3ab7c35d
jsump <libbyn79@gmail.com>
2023-08-12 16:09:07 -0700

    Print different parts of text

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+string = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = string[39:66] + " " + string[:6]
+print(str)
