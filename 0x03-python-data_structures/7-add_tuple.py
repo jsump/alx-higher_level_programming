c28d1c09ae9868916d227670c1eaa3e5219bae76
jsump <libbyn79@gmail.com>
2023-08-14 05:49:48 -0700

    Add 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a += (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b += (0,) * (2 - len(tuple_b))
+
+    a, b = tuple_a[:2]
+    c, d = tuple_b[:2]
+
+    return (a + c, b + d)
