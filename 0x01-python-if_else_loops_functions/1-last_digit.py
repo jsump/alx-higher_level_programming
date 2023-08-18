b3f48139c9b29519cf648540089e243b3b00e808
jsump <libbyn79@gmail.com>
2023-08-18 06:33:37 -0700

    Print last digit of the # stored in the var number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,8 @@ number = random.randint(-10000, 10000)
 print("Last digit of", number, "is", abs(number) % 10, end=" ")
 
 if abs(number) % 10 > 5:
-    print("and is grater than 5")
+    print("and is greater than 5")
 elif abs(number) % 10 == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
-print()
