99ecde1b41d3cdf77e7c5558958fbc3611ed1104
jsump <libbyn79@gmail.com>
2023-08-29 03:31:05 -0700

    Added final documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -7,6 +7,8 @@ The Square class is defined in this module to provide information
 for creating square-related functionality. While the class is currectly empty,
 it can be a starting point for building more comlex features in the future.
 """
+
+
 class Square:
     """
     This is a class that represents a square.
@@ -15,5 +17,13 @@ class Square:
     This means it does not contain any methods or attributes.
     It is meant to serve as a foundation for building more complex /
     square-related functionality in the future.
+    """
+    pass
+
+
+def possible_function(variable):
+    """
+    This is the function that would carry out any operation
+    associated with a sqaure
     """
     pass
