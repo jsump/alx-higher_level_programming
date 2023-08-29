8bd6e8a14cc635028bc903a715d8fc33a297a956
jsump <libbyn79@gmail.com>
2023-08-29 03:16:50 -0700

    Write an empty class Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 class Square:
-    """This is a class that represents a square
+    """
+    This is a class that represents a square.
+
+    This class is empty.
+    This means it does not contain any methods or attributes.
+    It is meant to serve as a foundation for building more complex /
+    square-related functionality in the future.
     """
     pass
