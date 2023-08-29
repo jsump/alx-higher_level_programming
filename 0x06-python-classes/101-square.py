9bb2c97a8c0b1579a21c0b77268ecfcacd806265
jsump <libbyn79@gmail.com>
2023-08-29 08:26:53 -0700

    Class Square printing Square with my_print() behaviour

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -89,7 +89,7 @@ class Square:
         """
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(isinstance(val, int) and val >= 0 for i in value):
+        if not all(isinstance(val, int) and val >= 0 for val in value):
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = value
