f95361906ecc6eaa263a8db8539b3ea0f1398b1d
jsump <libbyn79@gmail.com>
2023-08-29 11:58:10 -0700

    Task 8 edit

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -112,10 +112,9 @@ class Square:
         """
         if self.__size == 0:
             print()
-            return
 
-        for _ in range(self.__position[1]):
-            print()
-        for _ in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
