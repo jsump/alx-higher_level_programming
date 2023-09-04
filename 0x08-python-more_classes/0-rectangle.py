6ac1b3c219eb7fa78ca4b10862554948f2cac732
jsump <libbyn79@gmail.com>
2023-09-04 02:18:31 -0700

    Empty class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module: 0-rectangle
+
+This module contains  class that defines a rectangle
+"""
+class Rectangle:
+    """
+    This is an empty class that defines a rectangle
+    """
