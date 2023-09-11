e5b2c97887ce4756ab8b7a69b34a7863ca4de5b8
jsump <libbyn79@gmail.com>
2023-09-11 12:32:59 -0700

    Is object exactly an instance of a soecified class?

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,6 +5,8 @@ Module: 2-is_same_class
 this module contains a function that return a tatus if the object
 is exactly an instance of the specified class.
 """
+
+
 def is_same_class(obj, a_class):
     """
     This function returns True is the object is exactly and instance
