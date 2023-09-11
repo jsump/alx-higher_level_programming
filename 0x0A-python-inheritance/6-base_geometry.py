9ba8cf17759ef046b80203c0f8731086f6a409ae
jsump <libbyn79@gmail.com>
2023-09-11 14:16:39 -0700

    Raise Exception with message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module: 6-base_geometry
+
+This module contains a class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """
+    This is a class that creates the base for geometry
+    """
+
+    def area(self):
+        """
+        This functioncalculates the area.
+
+        Raises:
+            Exception: with message 'area() is not implemented'
+        """
+        raise Exception("area() is not implemented")
