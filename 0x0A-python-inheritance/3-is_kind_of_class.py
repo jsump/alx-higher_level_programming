d20b4eafb02e6452795ffd42b35bc51935f99602
jsump <libbyn79@gmail.com>
2023-09-11 12:40:45 -0700

    Is instance of a specified class, inherited or not

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pthon3
+"""
+Module: 3-is_kind_of_class
+
+This module contains a function that returns a status if the onject
+is an instance of or if the obj is an instance of a class inherited from
+the specified class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    This function returns True if the object is an instance of,
+    or if the object is an instance of a class inherited from
+    the specified class; otherwise False
+    """
+    return isinstance(obj, a_class)
