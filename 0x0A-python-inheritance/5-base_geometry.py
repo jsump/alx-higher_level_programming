e1cb305960d1fc3e418574e3e40db7c333414370
jsump <libbyn79@gmail.com>
2023-09-11 14:08:25 -0700

    Create an empty class BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module: 5-base_geometry
+
+This module contains an empty class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """
+    This is an empty class
+    """
+    pass
