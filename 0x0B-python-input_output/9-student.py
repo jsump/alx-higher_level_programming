29172f82e5e2b88b8c59f753774dcfab8a3f14ef
jsump <libbyn79@gmail.com>
2023-09-12 05:08:56 -0700

    Define a student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Module: 9-student
+
+This module contains a class Stdent that defines a student.
+"""
+
+
+def class_to_json(obj):
+    """
+    This function returns the dictionary description
+    with simple data structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object.
+    """
+    if isinstance(obj, list):
+        return [class_to_json(item) for item in obj]
+    elif isinstance(obj, dict):
+        return {key: class_to_json(value) for key, value in obj.items()}
+    elif isinstance(obj, (str, int, bool)):
+        return obj
+    else:
+        return {attr: class_to_json(getattr(obj, attr))
+                for attr in dir(obj) if not attr.startswith('__')}
+
+
+class Student:
+    """
+    This class defines a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        This function initializes the public instane attributes.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        This function retrieves a dictionary representation of
+        a student.
+        """
+        attributes = {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age
+        }
+        return class_to_json(attributes)
