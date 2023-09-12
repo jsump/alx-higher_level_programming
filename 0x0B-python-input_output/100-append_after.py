429d03df645df50b5791062e7aac0b12497934c2
jsump <libbyn79@gmail.com>
2023-09-12 06:14:39 -0700

    Insert line of text to a file w spec string

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,11 +12,11 @@ def append_after(filename="", search_string="", new_string=""):
     This function inserts a line of text to a file,
     after each line containing a specific string.
     """
-    with open(filename, "r+") as file:
+    with open(filename, "r") as file:
         lines = file.readlines()
-        file.seek(0)
+
+    with open(filename, "w") as file:
         for line in lines:
             file.write(line)
             if search_string in line:
                 file.write(new_string + "\n")
-        file.truncate()
