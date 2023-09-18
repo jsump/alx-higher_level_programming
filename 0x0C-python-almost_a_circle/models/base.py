00453766eb44ad5e7ecb077acafbcc2ba612d752
jsump <libbyn79@gmail.com>
2023-09-18 10:24:37 -0700

    Edit function to print NOF

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -80,7 +80,7 @@ class Base:
         """
         This function returns a list of instances.
         """
-        filename = cls.__name__ + ".json"
+        filename = f"{cls.__name__}.json"
         try:
             with open(filename, "r") as file:
                 data = json.load(file)
