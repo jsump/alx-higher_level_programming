ee37d6d0ea84eae4a1a294cd8e0c44aa19dfdd19
jsump <libbyn79@gmail.com>
2023-09-18 14:50:04 -0700

    Edit function to print NOFc

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -81,7 +81,7 @@ class Base:
         This function returns a list of instances by using from_json_sting
         and create methods.
         """
-        filename = "str{cls.__name__}.json"
+        filename = f"{cls.__name__}.json"
         try:
             with open(filename, "r") as file:
                 data = json.load(file)
