e8e6b60d2b927f32e8694a3586793fe9d003eab7
jsump <libbyn79@gmail.com>
2023-10-17 07:29:53 -0700

    Insert new row into table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Insert a nre row in the table first_table in DB hbtn_0c_0 in MySQL server
-INSERT INTO first_table
-VALUES(id, name)(89, 'BestSchool');
+INSERT INTO first_table (id, name)
+VALUES (89, 'BestSchool');
