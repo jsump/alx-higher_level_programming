c68547dbf05cfb6a71b42e12b087cbf29cd90c0c
jsump <libbyn79@gmail.com>
2023-10-17 08:03:30 -0700

    Cretate second_table with mult rows and create records

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- Create a second_table and add multiple rows
-CREATE IF NOT EXISTS second_table(
+CREATE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
-	score INT)
+	score INT
 );
 INSERT INTO second_table (id, name, score) 
 VALUES 
