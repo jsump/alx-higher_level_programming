aa2e892e31d7add906102d08df1e257b667408e4
jsump <libbyn79@gmail.com>
2023-10-18 07:11:07 -0700

    Create DB and table with attr with PK, AG and UNQ

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Create DB and table with unique, auto-generated attrubute that can't ne null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 	);
