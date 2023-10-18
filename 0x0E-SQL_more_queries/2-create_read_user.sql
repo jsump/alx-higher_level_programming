d1ba2c4244a2a9bfc90d01db9ce0da4e06310bad
jsump <libbyn79@gmail.com>
2023-10-18 06:25:38 -0700

    Create DB user and assign SELECT privilege

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Create the DB hbtn_0d_2 and user user_0d_2 with SELECT privilege on the DB
 -- Password set to user_0d_2_pwd
-CREATE IF NOT EXISTS DATABASE hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE IF NOT EXISTS USER 'user_0d_2'@'localhost' IDENTIFIED BY user_0d_2_pwd;
 GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
