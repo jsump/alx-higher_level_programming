0dbeb29e6395f6233afe28196156c9d2e6b37bd4
jsump <libbyn79@gmail.com>
2023-10-18 09:32:38 -0700

    Edit task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- Create DB and table with unique, auto-generated attrubute that can't ne null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
 	id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL
