98814262042eb6d4a1f1c0b83ab8273c218bd8b5
jsump <libbyn79@gmail.com>
2023-10-18 05:56:29 -0700

    Create user and grant all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create the MySQL server user user_0d_1 and give all privileges on server
 -- Password should be set too user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON "." TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
