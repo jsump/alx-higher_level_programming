010456e35a74a625cd2fddf2219b15516a12f78d
jsump <libbyn79@gmail.com>
2023-10-19 05:54:16 -0700

    Task 7 edit

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF EXISTS cities (
 	id INT UNIQUE auto_generated NOT NULL primary key,
-	state_id INT NOT NULL foreign key(states) references states(id),
+	state_id INT NOT NULL,
+	foreign key(states_id) references states(id),
 	name VARCHAR(256)
 	);
