21c60b63a7939d91536f2cc01aee7a9e5c634b4d
jsump <libbyn79@gmail.com>
2023-10-19 11:40:52 -0700

    List all shows and all genres linked to each

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- List all shows and all genres linked to that show
-SELECT tv_shows.title, tv_genres.name AS name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title, genre ASC;
+SELECT tv_shows.title, tv_genres.name AS name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title, name ASC;
