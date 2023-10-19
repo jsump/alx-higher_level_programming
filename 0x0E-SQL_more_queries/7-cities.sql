49a41030adff72521b64fb4183116f4d9e36c16d
jsump <libbyn79@gmail.com>
2023-10-19 06:04:31 -0700

    Task 7 edit

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- Creates DB and table in same DB
-CREATE DATABASE IF EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF EXISTS cities (
+CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE auto_generated NOT NULL primary key,
 	state_id INT NOT NULL,
 	foreign key(state_id) references states(id),
