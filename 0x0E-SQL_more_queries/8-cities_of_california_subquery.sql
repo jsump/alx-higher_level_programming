6e8027ba58075d3ed7d6e59f898f6f7eaf31d908
jsump <libbyn79@gmail.com>
2023-10-19 06:25:37 -0700

    List all cties of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- List all ccities of California
-SELECT id, name FROM cities WHERE state_id = state_id AND name = 'California' ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECTid FROM states WHERE name = 'California') ORDER BY id ASC;
