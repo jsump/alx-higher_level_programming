48ee933441edcd38ae51ff691bb92fdedaab68fe
jsump <libbyn79@gmail.com>
2023-10-19 12:32:20 -0700

    List shows by their rating in desc order

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,2 +1,2 @@
 -- List all shows by their rating
-SELECT tv_shows.title, SUM(hbtn_0d_tvshows_rate.rating) AS rating FROM tv_shows JOIN hbtn_0d_tvshows_rate ON tv_shows.id = hbtn_0d_tvshows_rate.show_id GROUP BY tv_shows.title ORDER BY rating DESC;
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id GROUP BY tv_shows.title ORDER BY rating DESC;
