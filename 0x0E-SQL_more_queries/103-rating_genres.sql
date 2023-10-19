2e17e90b56c7b0432b9c68c9f4b2c1b12ce22c84
jsump <libbyn79@gmail.com>
2023-10-19 12:41:10 -0700

    List all genres in DB by rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,2 +1,2 @@
 -- List all genres in database by their ratings
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating FROM tv_genres JOIN tv_show_genres ON tv_genred.id = tv_show_genres.genre_id JOIN tv_show_ratings.rate ON tv_show_genres.show_id = tv_show_ratings.show_id GROUP BY tv_genres.name ORDER BY rating DESC;
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id GROUP BY tv_genres.name ORDER BY rating DESC;
