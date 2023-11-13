868c764261f6ae5d08b15502a4a9f400e1f4c51d
jsump <libbyn79@gmail.com>
2023-11-13 09:28:19 -0800

    Add two integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const addition = (a, b) => {
+  return a + b;
+};
+const argsCount = process.argv.slice(2);
+const firstInteger = parseInt(argsCount[0]);
+const secondInteger = parseInt(argsCount[1]);
+
+if (!isNaN(firstInteger) && !isNaN(secondInteger)) {
+  console.log(addition(firstInteger, secondInteger));
+} else {
+  console.log('Nan');
+}
