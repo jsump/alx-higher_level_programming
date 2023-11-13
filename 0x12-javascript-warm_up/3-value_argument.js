a7d71857b4bd8dfde5b063862730b6f3fec55109
jsump <libbyn79@gmail.com>
2023-11-13 07:14:07 -0800

    Task 3 edit

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argsCount = process.argv.slice(2);
-if (argsCount.length === 0) {
+if (!argsCount[0]) {
   console.log('No argument');
 } else {
-  console.log(argsCount[0]);
+  console.log(...argsCount);
 }
