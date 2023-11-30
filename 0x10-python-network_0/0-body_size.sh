cbc0d80b48fcbd9fbc24dd81d800fad7c35a2fc3
jsump <libbyn79@gmail.com>
2023-11-30 03:26:02 -0800

    Task 0 edit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # This script takes ina a RUL, sends request o it and displays the size of body of reponse
-curl -s -w "%{size_download}" -o /dev/null "$1"
+curl -s -w "%{size_download}" -o /dev/null "$url"
