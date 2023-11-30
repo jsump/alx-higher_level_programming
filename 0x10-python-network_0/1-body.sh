1dca56b7b31f5f77fb7cc2014208b8ab8a99b988
jsump <libbyn79@gmail.com>
2023-11-30 04:06:02 -0800

    Task 1 edit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Take in a URL, send a GET re to it, display the body of response
-curl -s "$url"
+curl -s -o /dev/null -w "%{http_code}\n" "$url" | [ "$(cat)" == "200" ] && curl -s "$url"
