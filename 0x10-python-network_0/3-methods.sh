fdb97f380c8bd26233d49bab70a3cf853575f693
jsump <libbyn79@gmail.com>
2023-11-30 04:57:54 -0800

    Task 3 edit

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Take in URL, display all HHTP methods with request
-curl -s -I -X OPTIONS $1 | grep 'Allow:' | awk '{print $2}'
+curl -s -I -X OPTIONS $1 | grep -i 'Allow:' |cut -d' ' -f2-
