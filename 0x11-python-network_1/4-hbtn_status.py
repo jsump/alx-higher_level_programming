beec3c43884839951afbbdb9f0f4c696dbc65774
jsump <libbyn79@gmail.com>
2023-12-01 12:26:56 -0800

    Task 4 edit

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -18,7 +18,7 @@ def fetch_status():
 
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
-    print("\t - content: {}".format(response.text))
+    print("\t- content: {}".format(response.text))
 
 
 if __name__ == '__main__':
