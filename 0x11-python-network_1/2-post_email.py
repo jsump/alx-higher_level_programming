31c43fda544cfc18bb84ebd394cb64b86faa6bcf
jsump <libbyn79@gmail.com>
2023-12-01 09:29:20 -0800

    Post an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module: 2-post_email
+
+This module contains a function that displays the body of the response.
+"""
+
+
+import sys
+import urllib.request
+import urllib.parse
+
+
+def post_email():
+    """
+    This method takes in a url and email
+    Sends a POST to the passed url with email as a parameter
+    Displays the body of the response
+    """
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8') # Make data into bytes
+
+    reqst = urllib.request.Request(url, data=data, method='POST')
+
+    with urllib.request.urlopen(reqst) as response:
+        body = response.read()
+        body_str = body.decode('utf-8')
+
+        print(body_str)
+
+
+if __name__ == '__main__':
+    post_email()
