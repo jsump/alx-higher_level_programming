f1b8dc387c85475dad9374a139992e80a2e27f53
jsump <libbyn79@gmail.com>
2024-01-15 06:10:25 -0800

    Write string into file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,7 +7,5 @@ const content = process.argv[3]; // retrieve string to write from cmd line arg
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err); // print error of obj if occured
-  } else {
-    console.log('success'); // sucess message
   }
 });
