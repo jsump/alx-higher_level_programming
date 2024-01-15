01a196eace54f88b698f3cb0ff87007361ff4912
jsump <libbyn79@gmail.com>
2024-01-15 07:08:34 -0800

    Task 4 edit

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,6 @@
 const request = require('request');
 
 const apiUrl = process.argv[2]; // get api from cmd line arg
-const charId = 18; // wedge antilles character ID
 
 request.get(apiUrl, (error, response, body) => {
   if (error) {
@@ -10,7 +9,7 @@ request.get(apiUrl, (error, response, body) => {
   } else {
     const films = JSON.parse(body).results;
     const moviesWithCharID = films.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${charId}/`));
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/18/`));
     console.log(moviesWithCharID.length); // Print number of movies with Wedge Antilles
   }
 });
