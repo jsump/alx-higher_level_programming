a5bd3ecd0e8de27f540a1da1ecb27167dd68bd58
jsump <libbyn79@gmail.com>
2024-01-15 07:09:26 -0800

    Revert "Task 4 edit"
    This reverts commit 01a196eace54f88b698f3cb0ff87007361ff4912.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,6 +2,7 @@
 const request = require('request');
 
 const apiUrl = process.argv[2]; // get api from cmd line arg
+const charId = 18; // wedge antilles character ID
 
 request.get(apiUrl, (error, response, body) => {
   if (error) {
@@ -9,7 +10,7 @@ request.get(apiUrl, (error, response, body) => {
   } else {
     const films = JSON.parse(body).results;
     const moviesWithCharID = films.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/18/`));
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${charId}/`));
     console.log(moviesWithCharID.length); // Print number of movies with Wedge Antilles
   }
 });
