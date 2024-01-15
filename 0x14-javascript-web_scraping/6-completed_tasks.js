b9d31347a2fd5d4ee41b88b40d2938df468f9612
jsump <libbyn79@gmail.com>
2024-01-15 09:02:43 -0800

    Compute # of tasks completed by user

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2]; // get api from cmd line arg
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error); // print error if request fails
+  } else {
+    const todos = JSON.parse(body);
+    const tasksDoneByUser = todos.reduce((acc, todo) => {
+      if (todo.completed) {
+        if (acc[todo.userId]) {
+          acc[todo.userId]++;
+        } else {
+          acc[todo.userId] = 1;
+        }
+      }
+      return acc;
+    }, {});
+
+    for (const userId in tasksDoneByUser) {
+      console.log(`'${userId}': ${tasksDoneByUser[userId]}`);
+    }
+  }
+});
